d5b2be74ccc23b479c4436a91a045e5d3018829a
Peter <petergorman11@gmail.com>
2022-07-08 10:45:36 -0400

    Rename GridEntity to Grid and create base for grid entities

---
+++ code/Grid/Grid.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections.Generic;
+using CodeItOut.Grid.Traverser;
+using CodeItOut.Items;
+using CodeItOut.Utility;
+using Sandbox;
+
+namespace CodeItOut.Grid;
+
+public partial class Grid : Entity
+{
+	[Net] public IntVector2 CellSize { get; set; }
+	[Net] public IntVector2 Size { get; set; }
+	[Net] public IntVector2 StartPosition { get; set; }
+	
+	[Net] public GridTraverser Traverser { get; private set; }
+	[Net] public IList<GridCell> CellData { get; private set; }
+	[Net] public IDictionary<IntVector2, TraverserItem> Items { get; private set; }
+	[Net] public IList<GridEntity> Entities { get; private set; }
+
+	public Vector2 WorldCenter => new(Position.x + WorldWidth / 2, Position.y + WorldHeight / 2);
+	public float WorldWidth => Size.X * CellSize.X;
+	public float WorldHeight => Size.Y * CellSize.Y;
+
+	public override void Spawn()
+	{
+		base.Spawn();
+		
+		Traverser = new GridTraverser {Grid = this, Owner = this};
+	}
+	
+	public override void FrameSimulate( Client cl )
+	{
+		base.FrameSimulate( cl );
+		
+		for ( var x = 0; x < Size.X; x++ )
+		{
+			for ( var y = 0; y < Size.Y; y++ )
+			{
+				var cell = CellData[GetIndexAt( x, y )];
+				cell.DebugDraw();
+			}
+		}
+	}
+
+	public async void Run()
+	{
+		for ( var i = 0; i < Traverser.ActionCount; i++ )
+		{
+			await Traverser.RunAction( i );
+
+			foreach ( var entity in Entities )
+				await entity.RunAction( i );
+		}
+
+		await GameTask.DelaySeconds( 5 );
+		Reset();
+	}
+
+	public void Reset()
+	{
+		Host.AssertServer();
+		
+		Traverser.Reset();
+
+		foreach ( var (startPosition, item) in Items )
+		{
+			if ( !TryGetCellAt( startPosition.X, startPosition.Y, out var cellInfo ) )
+			{
+				Log.Error( "Failed to get items original position." );
+				continue;
+			}
+			
+			item.OnDrop( cellInfo );
+		}
+
+		foreach ( var cell in CellData )
+			cell.Reset();
+	}
+
+	public bool IsValidPosition( int x, int y )
+	{
+		if ( x < 0 || y < 0 )
+			return false;
+
+		if ( x > Size.X - 1 || y > Size.Y - 1 )
+			return false;
+
+		return true;
+	}
+
+	public void AddItemTo( int x, int y, TraverserItem item )
+	{
+		if ( !TryGetCellAt( x, y, out var cellInfo ) )
+		{
+			Log.Error( $"Failed to add item to {x}, {y} because the X, Y provided is not valid." );
+			return;
+		}
+
+		if ( cellInfo.GroundItem is not null )
+		{
+			Log.Error( $"Failed to add item to {x}, {y} because an item is already there." );
+		}
+
+		Items.Add( new IntVector2( x, y ), item );
+		item.OnDrop( cellInfo );
+	}
+
+	public void PlaceObjectAt( int x, int y, Direction objDirection, GridObject obj )
+	{
+		if ( !TryGetCellAt( x, y, out var cellInfo ) )
+		{
+			Log.Error( $"Failed to place object to {{x}}, {y} because the X, Y provided is not valid." );
+			return;
+		}
+
+		cellInfo.AddObject( objDirection, obj );
+	}
+
+	public bool TryGetCellAt( int x, int y, out GridCell gridCell )
+	{
+		if ( !IsValidPosition( x, y ) )
+		{
+			gridCell = null;
+			return false;
+		}
+
+		gridCell = CellData[GetIndexAt( x, y )];
+		return true;
+	}
+
+	public bool TryGetCellInDirection( int x, int y, Direction dir, out GridCell gridCell )
+	{
+		if ( !IsValidPosition( x, y ) )
+		{
+			gridCell = null;
+			return false;
+		}
+
+		var targetCellPosition = new IntVector2( x, y ) + dir.ToPosition();
+		return TryGetCellAt( targetCellPosition.X, targetCellPosition.Y, out gridCell );
+	}
+
+	private int GetIndexAt( int x, int y )
+	{
+		return x + Size.X * y;
+	}
+}
--- code/Grid/GridEntity.cs
+++ code/Grid/GridEntity.cs
@@ -1,129 +1,93 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using CodeItOut.Grid.Traverser;
-using CodeItOut.Items;
 using CodeItOut.Utility;
 using Sandbox;
 
 namespace CodeItOut.Grid;
 
-public partial class GridEntity : Entity
+public partial class GridEntity : AnimatedEntity
 {
-	[Net] public IntVector2 CellSize { get; set; }
-	[Net] public IntVector2 Size { get; set; }
-	[Net] public IntVector2 StartPosition { get; set; }
+	[Net] public Grid Grid { get; set; }
+	[Net] protected IntVector2 GridPosition { get; set; }
+	[Net] protected Direction FacingDirection { get; set; }
+
+	protected virtual Direction StartDirection => Direction.Up;
+
+	protected GridCell CurrentGridCell => Grid.TryGetCellAt( GridPosition.X, GridPosition.Y, out var cellInfo )
+		? cellInfo
+		: null;
+
+	public int ActionCount => _svActions.Count;
+	private readonly List<(TraverserAction, object[])> _svActions = new();
 	
-	[Net] public GridTraverser Traverser { get; private set; }
-	[Net] public IList<GridCell> CellData { get; private set; }
-	[Net] public IDictionary<IntVector2, TraverserItem> Items { get; private set; }
-
-	public Vector2 WorldCenter => new(Position.x + WorldWidth / 2, Position.y + WorldHeight / 2);
-	public float WorldWidth => Size.X * CellSize.X;
-	public float WorldHeight => Size.Y * CellSize.Y;
-
-	public override void Spawn()
-	{
-		base.Spawn();
-		
-		Traverser = new GridTraverser {Grid = this, Owner = this};
-	}
-
-	public void Reset()
+	public virtual void Reset()
 	{
 		Host.AssertServer();
 		
-		Traverser.Reset();
-
-		foreach ( var (startPosition, item) in Items )
-		{
-			if ( !TryGetCellAt( startPosition.X, startPosition.Y, out var cellInfo ) )
-			{
-				Log.Error( "Failed to get items original position." );
-				continue;
-			}
-			
-			item.OnDrop( cellInfo );
-		}
+		GridPosition = Grid.StartPosition;
+		FacingDirection = StartDirection;
+		_svActions.Clear();
 	}
 
-	public override void FrameSimulate( Client cl )
+	public void AddAction( TraverserAction action, params object[] args )
 	{
-		base.FrameSimulate( cl );
-		
-		for ( var x = 0; x < Size.X; x++ )
-		{
-			for ( var y = 0; y < Size.Y; y++ )
-			{
-				var cell = CellData[GetIndexAt( x, y )];
-				cell.DebugDraw();
-			}
-		}
+		Host.AssertServer();
+		_svActions.Add( (action, args) );
 	}
-	
-	public bool IsValidPosition( int x, int y )
+
+	public async Task<bool> RunAction( int actionIndex )
 	{
-		if ( x < 0 || y < 0 )
+		if ( actionIndex >= _svActions.Count )
 			return false;
 
-		if ( x > Size.X - 1 || y > Size.Y - 1 )
-			return false;
-
-		return true;
+		var (action, args) = _svActions[actionIndex];
+		return action switch
+		{
+			TraverserAction.MoveForward => await MoveForward(),
+			TraverserAction.TurnLeft => await TurnLeft(),
+			TraverserAction.TurnRight => await TurnRight(),
+			TraverserAction.UseObject => await UseObject(),
+			TraverserAction.UseItem => await UseItem( (double)args[0] ),
+			TraverserAction.PickupItem => await PickupItem(),
+			TraverserAction.DropItem => await DropItem( (double)args[0] ),
+			_ => throw new ArgumentOutOfRangeException( nameof(action), action, null )
+		};
 	}
 
-	public void AddItemTo( int x, int y, TraverserItem item )
+	protected virtual async Task<bool> MoveForward()
 	{
-		if ( !TryGetCellAt( x, y, out var cellInfo ) )
-		{
-			Log.Error( $"Failed to add item to {x}, {y} because the X, Y provided is not valid." );
-			return;
-		}
-
-		if ( cellInfo.GroundItem is not null )
-		{
-			Log.Error( $"Failed to add item to {x}, {y} because an item is already there." );
-		}
-
-		Items.Add( new IntVector2( x, y ), item );
-		item.OnDrop( cellInfo );
+		return false;
 	}
 
-	public void PlaceObjectAt( int x, int y, Direction objDirection, GridObject obj )
+	protected virtual async Task<bool> TurnLeft()
 	{
-		if ( !TryGetCellAt( x, y, out var cellInfo ) )
-		{
-			Log.Error( $"Failed to place object to {{x}}, {y} because the X, Y provided is not valid." );
-			return;
-		}
-
-		cellInfo.AddObject( objDirection, obj );
+		return false;
 	}
 
-	public bool TryGetCellAt( int x, int y, out GridCell gridCell )
+	protected virtual async Task<bool> TurnRight()
 	{
-		if ( !IsValidPosition( x, y ) )
-		{
-			gridCell = null;
-			return false;
-		}
-
-		gridCell = CellData[GetIndexAt( x, y )];
-		return true;
+		return false;
 	}
 
-	public bool TryGetCellInDirection( int x, int y, Direction dir, out GridCell gridCell )
+	protected virtual async Task<bool> UseObject()
 	{
-		if ( !IsValidPosition( x, y ) )
-		{
-			gridCell = null;
-			return false;
-		}
-
-		var targetCellPosition = new IntVector2( x, y ) + dir.ToPosition();
-		return TryGetCellAt( targetCellPosition.X, targetCellPosition.Y, out gridCell );
+		return false;
 	}
 
-	private int GetIndexAt( int x, int y )
+	protected virtual async Task<bool> UseItem( double itemIndex )
 	{
-		return x + Size.X * y;
+		return false;
+	}
+
+	protected virtual async Task<bool> PickupItem()
+	{
+		return false;
+	}
+
+	protected virtual async Task<bool> DropItem( double itemIndex )
+	{
+		return false;
 	}
 }
