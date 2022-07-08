f591bf2a7f897eb22ea067ca0a7ae85da2cb1e94
Peter <petergorman11@gmail.com>
2022-07-08 10:48:05 -0400

    Add floor object

---
+++ code/Grid/Objects/FloorObject.cs
@@ -0,0 +1,14 @@
+﻿namespace CodeItOut.Grid;
+
+public class FloorObject : GridObject
+{
+	protected override string ModelName => "models/maya_testcube_100.vmdl";
+	public override Vector3 Offset => new(0, 0, -100);
+
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		RenderColor = Color.Random;
+	}
+}
