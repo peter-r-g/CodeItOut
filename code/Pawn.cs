b257602da5703917f7ad7cc19b695b0b4230ea44
Peter <petergorman11@gmail.com>
2022-07-08 10:49:36 -0400

    Update debug level grid

--- code/Pawn.cs
+++ code/Pawn.cs
@@ -1,13 +1,12 @@
 ﻿using CodeItOut.Grid;
 using CodeItOut.Items;
-using CodeItOut.Utility;
 using Sandbox;
 
 namespace CodeItOut;
 
 public partial class Pawn : Player
 {
-	[Net] public GridEntity Grid { get; private set; }
+	[Net] public Grid.Grid Grid { get; private set; }
 
 	public override void Spawn()
 	{
@@ -16,20 +15,32 @@ public partial class Pawn : Player
 		Grid = new GridBuilder()
 			.WithOwner( this )
 			.WithWorldPosition( Vector3.Zero )
-			.WithSize( 5, 5 )
-			.WithStartPosition( 2, 0 )
+			.WithSize( 1, 5 )
+			.WithStartPosition( 0, 0 )
 			.WithCellSize( 100, 100 )
 			.Build();
 
-		Grid.AddItemTo( 2, 1, new KeyItem {KeyColor = Color.Green} );
-
-		for ( var i = 0; i < 5; i++ )
+		var floorColor = Color.White;
+		for ( var x = 0; x < Grid.Size.X; x++ )
 		{
-			Grid.PlaceObjectAt( 2, i, Direction.Left, new WallObject() );
-			Grid.PlaceObjectAt( 2, i, Direction.Right, new WallObject() );
+			for ( var y = 0; y < Grid.Size.Y; y++ )
+			{
+				var floor = new FloorObject {RenderColor = floorColor};
+				Grid.PlaceObjectAt( x, y, Direction.None, floor );
+				floorColor = floorColor == Color.White ? Color.Magenta : Color.White;
+			}
 		}
 
-		Grid.PlaceObjectAt( 2, 4, Direction.Up, new DoorObject {KeyColor = Color.Green} );
+		Grid.AddItemTo( 0, 2, new KeyItem {KeyColor = Color.Green} );
+
+		Grid.PlaceObjectAt( 0, 0, Direction.Down, new WallObject() );
+		for ( var i = 0; i < 5; i++ )
+		{
+			Grid.PlaceObjectAt( 0, i, Direction.Left, new WallObject() );
+			Grid.PlaceObjectAt( 0, i, Direction.Right, new WallObject() );
+		}
+
+		Grid.PlaceObjectAt( 0, 4, Direction.Up, new DoorObject {KeyColor = Color.Green} );
 
 		Animator = null;
 		CameraMode = new LookAtCamera
