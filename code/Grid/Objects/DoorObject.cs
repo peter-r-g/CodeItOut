75d07117854269d645576f1d140cfbe8b9fd511f
Peter <petergorman11@gmail.com>
2022-07-18 12:13:39 -0400

    Remove key color info from door

--- code/Grid/Objects/DoorObject.cs
+++ code/Grid/Objects/DoorObject.cs
@@ -9,19 +9,12 @@ public partial class DoorObject : GridObject
 	protected override string ModelName => "models/maya_testcube_100.vmdl";
 
 	[Net] public bool Open { get; set; }
-	[Net] public Color KeyColor { get; set; }
 
 	public override void Spawn()
 	{
 		base.Spawn();
 
 		Scale = 0.1f;
-	}
-
-	[Event.Tick.Server]
-	protected void Tick()
-	{
-		RenderColor = KeyColor;
 	}
 
 	public override bool Use( GridTraverser user, TraverserItem usedItem )
@@ -32,7 +25,7 @@ public partial class DoorObject : GridObject
 			return true;
 		}
 		
-		if ( usedItem is not KeyItem key || key.KeyColor != KeyColor )
+		if ( usedItem is not KeyItem )
 			return false;
 
 		Open = !Open;
@@ -55,6 +48,6 @@ public partial class DoorObject : GridObject
 	{
 		base.DebugDraw();
 		
-		DebugOverlay.Text( $"Door(Open: {Open})", Position, KeyColor, 0.1f );
+		DebugOverlay.Text( $"Door(Open: {Open})", Position, Color.White, 0.1f );
 	}
 }
