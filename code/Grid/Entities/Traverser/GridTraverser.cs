c6253e2da8322f9d1bb65c92f024569dd208c114
Peter <petergorman11@gmail.com>
2022-07-18 12:09:35 -0400

    Remove duplicated behaviour from GridTraverser

--- code/Grid/Entities/Traverser/GridTraverser.cs
+++ code/Grid/Entities/Traverser/GridTraverser.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using CodeItOut.Items;
 using CodeItOut.UI;
 using CodeItOut.Utility;
@@ -8,18 +6,8 @@ using Sandbox;
 
 namespace CodeItOut.Grid.Traverser;
 
-public partial class GridTraverser : GridEntity
+public class GridTraverser : GridEntity
 {
-	[Net] public IList<TraverserItem> Items { get; private set; }
-
-	private const float TurnTime = 0.3f;
-	private Direction _svPreviousDirection;
-	private TimeSince _svTimeSinceTurnStart;
-
-	private const float TravelTime = 1f;
-	private IntVector2 _svPreviousGridPosition;
-	private TimeSince _svTimeSinceMoveStart;
-	
 	public override void Spawn()
 	{
 		base.Spawn();
@@ -34,168 +22,112 @@ public partial class GridTraverser : GridEntity
 		TraverserHud.Instance.Traverser = this;
 	}
 
-	[Event.Tick.Server]
-	protected void Tick()
-	{
-		UpdatePosition();
-		UpdateRotation();
-	}
-
 	public override void Reset()
 	{
 		base.Reset();
-
-		_svPreviousGridPosition = GridPosition;
-		_svPreviousDirection = FacingDirection;
+		
+		Items.Clear();
 	}
 
-	private void UpdatePosition()
+	protected override ActionResult MoveForward()
 	{
-		Host.AssertServer();
+		if ( !GridMap.TryGetCellAt( GridPosition.X, GridPosition.Y, out var cellInfo ) )
+			return ActionResult.Fail();
+			
+		if ( !cellInfo.CanMove[Direction] )
+			return ActionResult.Fail();
 		
-		var previousPosition = _svPreviousGridPosition;
-		if ( !Grid.TryGetCellAt( previousPosition.X, previousPosition.Y, out var previousCellInfo ) )
+		var newGridPosition = GridPosition + Direction.ToPosition();
+		if ( !GridMap.TryGetCellAt( newGridPosition.X, newGridPosition.Y, out var targetCellInfo ) )
+			return ActionResult.Fail();
+		
+		if ( !targetCellInfo.CanMove[Direction.Opposite()] )
+			return ActionResult.Fail();
+
+		return ActionResult.Success( newGridPosition, Direction, ArraySegment<GridItem>.Empty,
+			ArraySegment<GridItem>.Empty );
+	}
+
+	protected override ActionResult TurnLeft()
+	{
+		var newDirection = (byte)Direction - 1;
+		if ( newDirection < (byte)Direction.Up )
+			newDirection = (byte)Direction.Left;
+
+		return ActionResult.Success( GridPosition, (Direction)newDirection, ArraySegment<GridItem>.Empty,
+			ArraySegment<GridItem>.Empty );
+	}
+
+	protected override ActionResult TurnRight()
+	{
+		var newDirection = (byte)Direction + 1;
+		if ( newDirection > (byte)Direction.Left )
+			newDirection = (byte)Direction.Up;
+
+		return ActionResult.Success( GridPosition, (Direction)newDirection, ArraySegment<GridItem>.Empty,
+			ArraySegment<GridItem>.Empty );
+	}
+
+	protected override ActionResult UseObject()
+	{
+		if ( !GridMap.TryGetCellAt( GridPosition.X, GridPosition.Y, out var cellInfo ) )
+			return ActionResult.Fail();
+
+		foreach ( var obj in cellInfo.GetObjectsInDirection( Direction ) )
 		{
-			Log.Error( $"Failed to get cell at {previousPosition.X}, {previousPosition.Y}" );
-			return;
-		}
-		
-		var currentPosition = GridPosition;
-		if ( !Grid.TryGetCellAt( currentPosition.X, currentPosition.Y, out var cellInfo ) )
-		{
-			Log.Error( $"Failed to get cell at {currentPosition.X}, {currentPosition.Y}" );
-			return;
-		}
-
-		Position = Vector3.Lerp( previousCellInfo.WorldPositionCenter, cellInfo.WorldPositionCenter,
-			_svTimeSinceMoveStart / TravelTime );
-	}
-
-	private void UpdateRotation()
-	{
-		Host.AssertServer();
-		
-		Rotation = Rotation.Lerp( _svPreviousDirection.ToRotation(), FacingDirection.ToRotation(),
-			_svTimeSinceTurnStart / TurnTime );
-	}
-	
-	protected override async Task<bool> TurnLeft()
-	{
-		Host.AssertServer();
-
-		var newValue = (byte)FacingDirection - 1;
-		if ( newValue < (byte)Direction.Up )
-			newValue = (byte)Direction.Left;
-
-		_svPreviousDirection = FacingDirection;
-		FacingDirection = (Direction)newValue;
-		_svTimeSinceTurnStart = 0;
-		await GameTask.DelaySeconds( TurnTime + 0.5f );
-		return true;
-	}
-
-	protected override async Task<bool> TurnRight()
-	{
-		Host.AssertServer();
-		
-		var newValue = (byte)FacingDirection + 1;
-		if ( newValue > (byte)Direction.Left )
-			newValue = (byte)Direction.Up;
-
-		_svPreviousDirection = FacingDirection;
-		FacingDirection = (Direction)newValue;
-		_svTimeSinceTurnStart = 0;
-		await GameTask.DelaySeconds( TurnTime + 0.5f );
-		return true;
-	}
-
-	protected override async Task<bool> MoveForward()
-	{
-		Host.AssertServer();
-		
-		var newGridPosition = GridPosition + FacingDirection.ToPosition();
-		newGridPosition = newGridPosition.Clamp( IntVector2.Zero, Grid.Size - IntVector2.One );
-
-		if ( GridPosition == newGridPosition )
-			return false;
-		
-		if ( !CurrentGridCell.CanMove[FacingDirection] )
-			return false;
-
-		_svPreviousGridPosition = GridPosition;
-		GridPosition = newGridPosition;
-		_svTimeSinceMoveStart = 0;
-		await GameTask.DelaySeconds( TravelTime + 0.5f );
-		return true;
-	}
-
-	protected override async Task<bool> UseObject()
-	{
-		Host.AssertServer();
-
-		var cellInfo = CurrentGridCell;
-		if ( cellInfo is null )
-			return false;
-		
-		foreach ( var obj in cellInfo.GetObjectsInDirection( FacingDirection ) )
-		{
-			if ( obj.Use( this, null ) )
-				return true;
+			if ( obj.Use( this, null, out _ ) )
+				return ActionResult.Success( GridPosition, Direction, ArraySegment<GridItem>.Empty,
+					ArraySegment<GridItem>.Empty );
 		}
 
-		return false;
+		return ActionResult.Fail();
 	}
 
-	protected override async Task<bool> UseItem( double itemIndex )
+	protected override ActionResult UseItem( double itemIndex )
 	{
-		Host.AssertServer();
-
-		var cellInfo = CurrentGridCell;
-		if ( cellInfo is null )
-			return false;
+		if ( !GridMap.TryGetCellAt( GridPosition.X, GridPosition.Y, out var cellInfo ) )
+			return ActionResult.Fail();
 		
 		if ( Items.Count < itemIndex )
-			return false;
+			return ActionResult.Fail();
 
-		foreach ( var obj in cellInfo.GetObjectsInDirection( FacingDirection ) )
+		foreach ( var obj in cellInfo.GetObjectsInDirection( Direction ) )
 		{
-			if ( obj.Use( this, Items[(int)itemIndex] ) )
-				return true;
+			var item = Items[(int)itemIndex];
+			if ( obj.Use( this, item, out var itemUsed ) )
+				return ActionResult.Success( GridPosition, Direction, ArraySegment<GridItem>.Empty,
+					itemUsed ? new[] {item} : ArraySegment<GridItem>.Empty );
 		}
 
-		return false;
+		return ActionResult.Fail();
 	}
 
-	protected override async Task<bool> PickupItem()
+	protected override ActionResult PickupItem()
 	{
-		Host.AssertServer();
-
-		var cellInfo = CurrentGridCell;
-		if ( cellInfo is null )
-			return false;
+		if ( !GridMap.TryGetCellAt( GridPosition.X, GridPosition.Y, out var cellInfo ) )
+			return ActionResult.Fail();
 
 		if ( cellInfo.GroundItem is null )
-			return false;
+			return ActionResult.Fail();
 
 		cellInfo.GroundItem.OnPickup( this );
-		return true;
+		return ActionResult.Success( GridPosition, Direction, new[] {cellInfo.GroundItem},
+			ArraySegment<GridItem>.Empty );
 	}
 
-	protected override async Task<bool> DropItem( double itemIndex )
+	protected override ActionResult DropItem( double itemIndex )
 	{
-		Host.AssertServer();
-
-		var cellInfo = CurrentGridCell;
-		if ( cellInfo is null )
-			return false;
+		if ( !GridMap.TryGetCellAt( GridPosition.X, GridPosition.Y, out var cellInfo ) )
+			return ActionResult.Fail();
 
 		if ( cellInfo.GroundItem is not null )
-			return false;
+			return ActionResult.Fail();
 
 		if ( Items.Count < itemIndex )
-			return false;
+			return ActionResult.Fail();
 		
 		Items[(int)itemIndex].OnDrop( cellInfo );
-		return true;
+		return ActionResult.Success( GridPosition, Direction, ArraySegment<GridItem>.Empty,
+			new[] {cellInfo.GroundItem} );
 	}
 }
