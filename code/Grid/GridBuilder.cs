616fada0d8e4d2a07efa8302d46f05f6c79c2f7a
Peter <petergorman11@gmail.com>
2022-07-18 12:08:22 -0400

    Add more methods and build logic to GridBuilder

--- code/Grid/GridBuilder.cs
+++ code/Grid/GridBuilder.cs
@@ -1,15 +1,19 @@
-﻿using CodeItOut.Utility;
-using Sandbox;
+﻿using System;
+using System.Collections.Generic;
+using CodeItOut.Items;
+using CodeItOut.Utility;
 
 namespace CodeItOut.Grid;
 
 public class GridBuilder
 {
 	private IntVector2 _cellSize;
-	private Entity _owner;
 	private IntVector2 _size;
 	private IntVector2 _startPosition;
 	private Vector3 _worldPosition;
+
+	private readonly Dictionary<(int, int), Type> _items = new();
+	private readonly Dictionary<(int, int), List<(Type, Direction)>> _objects = new();
 	
 	public GridBuilder WithCellSize( IntVector2 cellSize )
 	{
@@ -20,12 +24,6 @@ public class GridBuilder
 	public GridBuilder WithCellSize( int x, int y )
 	{
 		_cellSize = new IntVector2( x, y );
-		return this;
-	}
-
-	public GridBuilder WithOwner( Entity owner )
-	{
-		_owner = owner;
 		return this;
 	}
 
@@ -58,13 +56,27 @@ public class GridBuilder
 		_worldPosition = position;
 		return this;
 	}
-
-	public Grid Build()
+	
+	public GridBuilder AddItem( int x, int y, Type gridItemType )
 	{
-		var grid = new Grid
+		_items.Add( (x, y), gridItemType );
+		return this;
+	}
+
+	public GridBuilder AddObject( int x, int y, Type gridObjectType, Direction direction = Direction.None )
+	{
+		if ( !_objects.ContainsKey( (x, y) ) )
+			_objects.Add( (x, y), new List<(Type, Direction)>() );
+		
+		_objects[(x, y)].Add( (gridObjectType, direction) );
+		return this;
+	}
+
+	public GridMap Build()
+	{
+		var grid = new GridMap
 		{
 			CellSize = _cellSize,
-			Owner = _owner,
 			Position = _worldPosition,
 			Size = _size,
 			StartPosition = _startPosition
@@ -76,7 +88,7 @@ public class GridBuilder
 			{
 				grid.CellData.Add( new GridCell
 				{
-					Grid = grid,
+					GridMap = grid,
 					GridPosition = new IntVector2( x, y ),
 					CanMove =
 					{
@@ -86,6 +98,21 @@ public class GridBuilder
 						[Direction.Left] = grid.IsValidPosition( x - 1, y )
 					}
 				} );
+				
+				if ( _items.TryGetValue( (x, y), out var itemType ) )
+					grid.AddItemTo( x, y, TypeLibrary.Create<GridItem>( itemType ) );
+			}
+		}
+
+		for ( var x = 0; x < grid.Size.X; x++ )
+		{
+			for ( var y = 0; y < grid.Size.Y; y++ )
+			{
+				if ( _objects.TryGetValue( (x, y), out var objects ) )
+				{
+					foreach ( var pair in objects )
+						grid.PlaceObjectAt( x, y, pair.Item2, TypeLibrary.Create<GridObject>( pair.Item1 ) );
+				}
 			}
 		}
 
