b8e137b2cd26fa2ac939b27230e3bafaa611fbaf
Peter <petergorman11@gmail.com>
2022-07-19 15:54:09 -0400

    Add back floor color randomization

--- code/Grid/Objects/FloorObject.cs
+++ code/Grid/Objects/FloorObject.cs
@@ -4,4 +4,14 @@ public class FloorObject : GridObject
 {
 	protected override string ModelName => "models/maya_testcube_100.vmdl";
 	public override Vector3 Offset => new(0, 0, -100);
+
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		do
+		{
+			RenderColor = Color.Random;
+		} while ( RenderColor == Color.Red );
+	}
 }
