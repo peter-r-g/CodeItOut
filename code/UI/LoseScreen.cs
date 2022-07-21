ba403d1bd18ae65c6426bc7d7b219abe9ae55eda
Peter <petergorman11@gmail.com>
2022-07-21 13:24:21 -0400

    Add lose screen

---
+++ code/UI/LoseScreen.cs
@@ -0,0 +1,14 @@
+﻿using CodeItOut.Grid;
+using Sandbox;
+using Sandbox.UI;
+
+namespace CodeItOut;
+
+[UseTemplate]
+public class LoseScreen : Panel
+{
+	public LoseScreen()
+	{
+		BindClass( "hidden", () => (Local.Pawn as Pawn)?.Map?.State != MapState.Lost );
+	}
+}
