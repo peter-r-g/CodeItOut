ca4d498fd10f3c81de76846ec69571822a4a3a56
Peter <petergorman11@gmail.com>
2022-07-21 17:20:42 -0400

    Add screen to show in the running state

---
+++ code/UI/RunningScreen.cs
@@ -0,0 +1,14 @@
+﻿using CodeItOut.Grid;
+using Sandbox;
+using Sandbox.UI;
+
+namespace CodeItOut;
+
+[UseTemplate]
+public class RunningScreen : Panel
+{
+	public RunningScreen()
+	{
+		BindClass( "hidden", () => (Local.Pawn as Pawn)?.Map?.State != MapState.Running );
+	}
+}
