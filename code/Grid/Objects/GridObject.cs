b766dc99567e512fafde9a54fe57d44d6c2b8774
Peter <petergorman11@gmail.com>
2022-07-21 00:06:19 -0400

    Add GridMap and GridCell reference to GridObject

--- code/Grid/Objects/GridObject.cs
+++ code/Grid/Objects/GridObject.cs
@@ -1,5 +1,4 @@
-﻿using CodeItOut.Grid.Traverser;
-using CodeItOut.Items;
+﻿using CodeItOut.Items;
 using Sandbox;
 
 namespace CodeItOut.Grid;
@@ -9,6 +8,8 @@ public partial class GridObject : ModelEntity
 	protected virtual string ModelName => "";
 	public virtual Vector3 Offset => Vector3.Zero;
 
+	[Net] public GridMap GridMap { get; set; }
+	[Net] public GridCell Cell { get; set; }
 	[Net] public Direction Direction { get; set; }
 
 	public override void Spawn()
