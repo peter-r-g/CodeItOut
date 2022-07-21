8820f7dfdef4d63403c808d48ff3207edea4ecdd
Peter <petergorman11@gmail.com>
2022-07-21 13:24:15 -0400

    Add win screen

---
+++ code/UI/WinScreen.cs
@@ -0,0 +1,14 @@
+﻿using CodeItOut.Grid;
+using Sandbox;
+using Sandbox.UI;
+
+namespace CodeItOut;
+
+[UseTemplate]
+public class WinScreen : Panel
+{
+	public WinScreen()
+	{
+		BindClass( "hidden", () => (Local.Pawn as Pawn)?.Map?.State != MapState.Won );
+	}
+}
