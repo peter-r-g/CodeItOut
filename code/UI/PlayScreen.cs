52b627e0be18af6dce6987c87501ae9d2336a3c8
Peter <petergorman11@gmail.com>
2022-07-21 17:20:31 -0400

    Add screen to show when in the not started state

---
+++ code/UI/PlayScreen.cs
@@ -0,0 +1,14 @@
+﻿using CodeItOut.Grid;
+using Sandbox;
+using Sandbox.UI;
+
+namespace CodeItOut;
+
+[UseTemplate]
+public class PlayScreen : Panel
+{
+	public PlayScreen()
+	{
+		BindClass( "hidden", () => (Local.Pawn as Pawn)?.Map?.State != MapState.NotStarted );
+	}
+}
